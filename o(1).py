3156e593c0715fefc7efe664502116f3b1447875
Tanvi <145559687+TanviS001@users.noreply.github.com>
2024-11-01 09:47:39 +0530

    Create o(1).py

---
+++ o(1).py
@@ -0,0 +1,8 @@
+# O(1)
+
+def add_self(n):
+  return n+n+n
+
+# constant time complexity. 
+# It means an algorithm or operation runs in the same amount of time, regardless of the input size. 
+# Quick and efficient
